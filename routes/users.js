f09a3b71a06a847ae3763dd480c43b847ff9a52b
zhanghang <zhangxing@licai.com>
2016-12-01 16:50:38 +0800

    add test and update view
    add test and update view

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var controller=require('../controller/user.server.controller');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users',controller.getAllUsers);
+
+
+router.get('/login',function (req,res) {
+   res.render('login',{title:'登录'});
 });
+
+/*用户登录*/
+router.post('/login',controller.findByUserName);
+
+router.get('/register',function (req,res,next) {
+   res.render('register',{title:'注册'})
+});
+
+router.post('/register',controller.create);
 
 module.exports = router;
---
+++ test/test_cryto.js
@@ -0,0 +1,4 @@
+/**
+ * Created by zhangh on 2016/11/30.
+ */
+console.log(new Date()-0);
