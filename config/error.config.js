4d99d28df44be800c9544c1ae7988e4931fd799d
zhanghang <zhangxing@licai.com>
2016-12-02 15:59:15 +0800

    new file:   config/error.config.js 	new file:   models/comment.server.model.js 	modified:   common/server.common.js 	modified:   config/error.config.js 	modified:   config/mongoose.js 	modified:   controller/news.server.controller.js 	modified:   controller/user.server.controller.js 	modified:   models/comment.server.model.js 	modified:   models/news.server.model.js 	modified:   models/user.server.model.js 	modified:   package.json 	modified:   test/test_cryto.js

---
+++ config/error.config.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zhangh on 2016/12/2.
+ */
---
+++ models/comment.server.model.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangh on 2016/12/2.
+ */
+var mongoose=require('mongoose');
+
+var comment=new mongoose.Schema({
+
+
+});
+
+module.exports=comment
